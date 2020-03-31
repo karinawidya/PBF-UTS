70f97bc0721db00b3dc11e20e5c71002c9d414a2
karinawidya <karinawidya9999@gmail.com>
2020-03-31 19:24:27 +0700

    membuat tampilan dan list dari tourData

---
+++ src/components/tourData.js
@@ -0,0 +1,58 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Bali",
+    img: "./img/bali.jpg",
+    name: "Pesona Indonesia Bali",
+    info:
+      "Dengan menawarkan beberapa tour wisata menunjukkan eksotisan Bali, diantaranya Pantai Kuta, tanah Lot, Desa Panglipuran"
+  },
+  {
+    id: 2,
+    city: "Banyuwangi",
+    img: "./img/banyuwangi.jpg",
+    name: "Pesona Indonesia Banyuwangi",
+    info:
+      "Pesona Banyuwangi diantaranya Kawah Ijen, Pulau Merah, Taman Nasional Alas Purwo"
+  },
+  {
+    id: 3,
+    city: "Sumba",
+    img: "./img/sumba.jpg",
+    name: "Pesona Indonesia Sumba",
+    info:
+      "Siapa yang tidak tahu Sumba dengan keindahan alamnya, Bukit Tarinding, Bukit Tanarara"
+  },
+  {
+    id: 4,
+    city: "Yogyakarta",
+    img: "./img/yogyakarta.jpeg",
+    name: "Pesona Indonesia Yogyakarta",
+    info:
+      "Salah satu Daerah Istimewa di Indonesia, tempat yang gak akan mengenalkan rasa bosan. Tugu, Malioboro, Taman Sari"
+  },
+  {
+    id: 5,
+    city: "Jakarta",
+    img: "./img/jakarta.jpg",
+    name: "Pesona Indonesia Jakarta",
+    info:
+      "Ibu Kota Indonesia dikenal dengan kepadatan penduduknya, namun selalu indah dengan banyaknya kendaraan memadati jalan, dan juga tentunya ada wisata diantaranya Monas, Taman Mini Indonesia, Ancol"
+  },
+  {
+    id: 6,
+    city: "Bromo",
+    img: "./img/bromo.jpg",
+    name: "Pesona Indonesia Bromo",
+    info:
+      "Bromo selalu memberikan pesona sunrise dan sunsetnya, include pasir berbisik, bukit teletubbies, dan penanjakan bromo"
+  },
+  {
+    id: 7,
+    city: "Bandung",
+    img: "./img/bandung.jpg",
+    name: "Pesona Indonesia Kawah Putih Bandung",
+    info:
+      "Selain kawah ijen Indonesia juga memiliki Kawah Putih yang berada di Bandung, keindahannya pun tak kalah drai Kawah Ijen dan sudah dikenal oleh seluruh kalangan"
+  }
+];
